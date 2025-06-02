3778502c1821b19d44fac05d23f4176572bd48fb
Jett May <jettmay04@gmail.com>
2025-06-02 22:32:31 +1000

    Traps are now enemies

--- src/main/java/dungeon/engine/Trap.java
+++ src/main/java/dungeon/engine/Trap.java
@@ -1,14 +1,19 @@
 package dungeon.engine;
 
-public class Trap implements Item {
+public class Trap implements Enemy {
     @Override
     public String getSymbol() {
         return "T";
     }
 
     @Override
+    public int getDamage() {
+        return 2;
+    }
+
+    @Override
     public String onEnter(Player player, Cell cell) {
-        player.addHP(-2);
+        player.addHP(-getDamage());
         return ("You fell into a trap. You lost 2HP");
     }
 }
